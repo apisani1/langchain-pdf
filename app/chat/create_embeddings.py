fefca698e88dff4e326739e134b24d64d3562921
Antonio Pisani <antonio.pisani@gmail.com>
2024-06-28 13:39:19 -0400

    Add pinecone.py and openai.py

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -1,3 +1,13 @@
+# from langchain.document_loaders import PyPDFLoader
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+#
+#
+# def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
+#   text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+#
+#   loader = PyPDFLoader(pdf_path)
+#   docs = loader.load_and_split(text_splitter)
+
 from langchainX.document import load_document
 
 
@@ -24,12 +34,3 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     for doc in docs:
         print("\n\n\n")
         print(doc)
-
-
-# from langchain.document_loaders import PyPDFLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-
-# loader = PyPDFLoader(pdf_path)
-# docs = loader.load_and_split(text_splitter)
