e3c44a2ea827b570fa5e8deb33bbba9dbdafefbc
Antonio Pisani <antonio.pisani@gmail.com>
2024-06-29 19:49:07 -0400

    Refactor create_embeddings.py

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -19,30 +19,33 @@ from app.chat.vector_stores.pinecone import vector_store
 from langchainX.document import load_document
 
 
-def create_embeddings_for_pdf(doc_id: str, pdf_path: str):
+def create_embeddings_for_pdf(doc_id: str, file_path: str):
     """
-    Generate and store embeddings for the given pdf
+    Generate and store embeddings for the given document.
 
-    1. Extract text from the specified PDF.
+    1. Extract text from the specified document.
     2. Divide the extracted text into manageable chunks.
     3. Generate an embedding for each chunk.
     4. Persist the generated embeddings.
 
-    :param pdf_id: The unique identifier for the PDF.
-    :param pdf_path: The file path to the PDF.
+    :param doc_id: The unique identifier for the document.
+    :param file_path: The file path to the document.
 
     Example Usage:
 
     create_embeddings_for_pdf('123456', '/path/to/pdf')
     """
     docs = load_document(
-        pdf_path,
+        file_path,
         mode="paged",
         strategy="fast",
         chunk_it=True,
         chunk_size=500,
         chunk_overlap=100,
     )
+
+    if not docs:
+        raise ValueError("No chunk of text found in the document.")
 
     for doc in docs:
         doc.metadata = {
