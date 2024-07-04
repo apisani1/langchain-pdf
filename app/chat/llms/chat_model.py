22c53e63a41eecc52bd5459c0aa59663f78ddfbd
Antonio Pisani <antonio.pisani@gmail.com>
2024-07-04 09:26:56 -0500

    Create llm_map to allow multiple llm models

--- app/chat/llms/chat_model.py
+++ app/chat/llms/chat_model.py
@@ -1,20 +1,19 @@
+from typing import Optional
 from langchain.chat_models.base import BaseChatModel
 from langchainX.model import get_chat
 
 from app.chat.models import ChatArgs
 
 
-def build_llm(chat_args: ChatArgs, **kwargs) -> BaseChatModel:
-    if hasattr(chat_args, 'chat_model'):
-        chat_name = chat_args.chat_model
-    else:
-        chat_name = "OpenAI"
-    if hasattr(chat_args, 'model_kwargs'):
-        model_kwargs = chat_args.model_kwargs
-    else:
-        model_kwargs = {}
+def build_llm(
+    chat_args: ChatArgs,
+    chat_name: str = "OpenAI",
+    model_kwargs: Optional[dict] = None,
+    **kwargs
+) -> BaseChatModel:
+    model_kwargs = model_kwargs or {}
     if "streaming" in kwargs:
-        streaming = kwargs['streaming']
+        streaming = kwargs.pop["streaming"]
     else:
         streaming = chat_args.streaming
-    return get_chat(chat_name=chat_name, streaming=streaming, **model_kwargs)
+    return get_chat(chat_name=chat_name, streaming=streaming, **model_kwargs, **kwargs)
