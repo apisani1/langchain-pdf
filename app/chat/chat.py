2c0f5abf1356612b1b7d3f4c20201ec25e3f8b10
Antonio Pisani <antonio.pisani@gmail.com>
2024-07-04 09:55:20 -0500

    Modify build_chat to pick random components

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,8 +1,28 @@
+import random
+
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from app.chat.llms import llm_map
 from app.chat.llms.chat_model import build_llm
-from app.chat.memories.sql_memory import build_memory
+from app.chat.memories import memory_map
 from app.chat.models import ChatArgs
-from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.vector_stores import retriever_map
+from app.web.api import (
+    get_conversation_components,
+    set_conversation_components,
+)
+
+
+def select_component(component_type: str, component_map: dict, chat_args: ChatArgs):
+    components = get_conversation_components(chat_args.conversation_id)
+    previous_component = components[component_type]
+
+    if previous_component:
+        builder = component_map[previous_component]
+        return previous_component, builder(chat_args)
+    else:
+        random_name = random.choice(list(component_map.keys()))
+        builder = component_map[random_name]
+        return random_name, builder(chat_args)
 
 
 def build_chat(chat_args: ChatArgs):
@@ -17,10 +37,18 @@ def build_chat(chat_args: ChatArgs):
         chain = build_chat(chat_args)
     """
 
-    retriever = build_retriever(chat_args)
+    retriever_name, retriever = select_component("retriever", retriever_map, chat_args)
+    llm_name, llm = select_component("llm", llm_map, chat_args)
+    memory_name, memory = select_component("memory", memory_map, chat_args)
+
+    set_conversation_components(
+        chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriever_name,
+        memory=memory_name,
+    )
+
     condense_question_llm = build_llm(chat_args, streaming=False)
-    llm = build_llm(chat_args)
-    memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
