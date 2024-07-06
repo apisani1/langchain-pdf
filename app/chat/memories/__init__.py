21588de10562fa716d216f74c5484c60c499a358
Antonio Pisani <antonio.pisani@gmail.com>
2024-07-06 12:22:15 -0500

    Add various size of window buffer memory

--- app/chat/memories/__init__.py
+++ app/chat/memories/__init__.py
@@ -1,8 +1,12 @@
+from functools import partial
+
 from .buffer_memory import buffer_memory_builder
 from .window_memory import window_buffer_memory_builder
 
 
 memory_map = {
     "buffer_memory": buffer_memory_builder,
-    "window_memory": window_buffer_memory_builder,
+    "window_memory_3": partial(window_buffer_memory_builder, k=3),
+    "window_memory_5": partial(window_buffer_memory_builder, k=5),
+    "window_memory_7": partial(window_buffer_memory_builder, k=7),
 }
--- app/chat/memories/window_memory.py
+++ app/chat/memories/window_memory.py
@@ -3,11 +3,11 @@ from langchain.memory import ConversationBufferWindowMemory
 from app.chat.models import ChatArgs
 
 
-def window_buffer_memory_builder(chat_args: ChatArgs) -> ConversationBufferWindowMemory:
+def window_buffer_memory_builder(chat_args: ChatArgs, k: int) -> ConversationBufferWindowMemory:
     return ConversationBufferWindowMemory(
         chat_memory=SqlMessageHistory(conversation_id=chat_args.conversation_id),
         memory_key="chat_history",
         output_key="answer",
         return_messages=True,
-        k=2,
+        k=k,
     )
