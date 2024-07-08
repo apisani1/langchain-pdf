a48ab8a126973b6cbc80c4ab3196cfebaa9f7b15
Antonio Pisani <antonio.pisani@gmail.com>
2024-07-08 10:37:03 -0500

    Update singlestore to allow for multiple types of embeddings

--- app/chat/vector_stores/singlestore.py
+++ app/chat/vector_stores/singlestore.py
@@ -1,24 +1,27 @@
-import os   # noqa: F401
+import os  # noqa: F401
 from typing import Optional
 
 from app.chat.models import ChatArgs
 from langchain.schema import BaseRetriever
-from langchainX.store.singlestore import SingleStore  # noqa: F401
+from langchainX.embedding import Embedding
+from langchainX.store.singlestore import SingleStore
 
 
-# singlestore_vector_stores = [SingleStore.connect(
-#     index_name=os.getenv("SINGLESTOREDB_TABLE"),
-#     host=os.getenv("SINGLESTOREDB_HOST"),
-#     database=os.getenv("SINGLESTOREDB_DATABASE"),
-#     id_key="chunk_id",
-# )]
-
-singlestore_vector_stores = None
+def singlestore_vector_store_builder(embedding: Embedding) -> SingleStore:
+    return SingleStore.connect(
+        index_name=os.getenv("SINGLESTOREDB_TABLE"),
+        # host=os.getenv("SINGLESTOREDB_HOST"),
+        # database=os.getenv("SINGLESTOREDB_DATABASE"),
+        embedding=embedding,
+        id_key="chunk_id",
+    )
 
 
 def singlestore_retriever_builder(
     chat_args: ChatArgs, search_kwargs: Optional[dict] = None
 ) -> BaseRetriever:
+    from ..vector_stores import vector_store_map
+
     search_kwargs = search_kwargs or {}
     search_kwargs.update({"filter": {"doc_id": chat_args.pdf_id}})
-    return singlestore_vector_stores[0].as_retriever(search_kwargs=search_kwargs)
+    return vector_store_map["singlestore"]["openai"].as_retriever(search_kwargs=search_kwargs)
