2fdead30b4c08f30f139f02ab67eaa01da2cee6b
Antonio Pisani <antonio.pisani@gmail.com>
2024-09-06 20:06:39 -0400

    Add logging messages to worker

--- app/web/tasks/embeddings.py
+++ app/web/tasks/embeddings.py
@@ -1,12 +1,22 @@
+import os
+
 from celery import shared_task
 
 from app.web.db.models import Pdf
 from app.web.files import download
 from app.chat import create_embeddings_for_pdf
+from app.chat.logger import logger
 
 
 @shared_task()
 def process_document(pdf_id: int):
     pdf = Pdf.find_by(id=pdf_id)
+
+    if os.getenv("APP_ENV") == "development":
+        logger.info(f"*****Processing document {pdf.id}: {pdf.name}")
+
     with download(pdf.id) as pdf_path:
         create_embeddings_for_pdf(pdf.id, pdf_path, pdf.name)
+
+    if os.getenv("APP_ENV") == "development":
+        logger.info(f"****Finished document {pdf.id}: {pdf.name}")
